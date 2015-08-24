c6affd4b183a1906fbda8a3ce3acfa39e7893fc8
Daniel Jimenez <daniel.j.jimenez@gmail.com>
2015-08-24 12:00:07 -0500

    Fix metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,11 @@
-name             'sprout-elasticsearch'
+name             'sprout-nginx'
 maintainer       'Foreflight'
 maintainer_email 'danielj@foreflight.com'
 license          'MIT'
-description      'Recipes to install and configure ElasticSearch on OS X'
+description      'Recipes to install and configure Nginx on OS X'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 supports         'mac_os_x'
 depends          'homebrew', '>= 1.5.4'
 depends          'sprout-base'
-depends          'java'
